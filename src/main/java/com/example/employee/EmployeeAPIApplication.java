c387c938a049d41709d353f80889d29592ce2635
jiaoqiqi <jiaoqiqi977@163.com>
2019-05-07 13:02:26 +0800

    complete jpa sql
    Signed-off-by: jiaoqiqi <jiaoqiqi977@163.com>

--- src/main/java/com/example/employee/EmployeeAPIApplication.java
+++ src/main/java/com/example/employee/EmployeeAPIApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EmployeeAPIApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(EmployeeAPIApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(EmployeeAPIApplication.class, args);
+  }
 }
--- src/main/resources/db/migration/V1__Create_table.sql
+++ src/main/resources/db/migration/V1__Create_table.sql
@@ -1,15 +1,19 @@
-CREATE TABLE Company(
-    id INT PRIMARY KEY,
-    companyName VARCHAR(255) NOT NULL,
-    employeesNumber INT NOT NULL
-) engine=InnoDB DEFAULT CHARSET = gbk;
+CREATE TABLE Company
+(
+  id              INT PRIMARY KEY,
+  companyName     VARCHAR(255) NOT NULL,
+  employeesNumber INT          NOT NULL
+) engine = InnoDB
+  DEFAULT CHARSET = gbk;
 
-CREATE TABLE Employee(
-    id INT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    age INT NOT NULL,
-    gender VARCHAR(10),
-    companyId INT NOT NULL,
-    salary INT NOT NULL
-) engine=InnoDB DEFAULT CHARSET = utf8;
+CREATE TABLE Employee
+(
+  id        INT PRIMARY KEY,
+  name      VARCHAR(255) NOT NULL,
+  age       INT          NOT NULL,
+  gender    VARCHAR(10),
+  companyId INT          NOT NULL,
+  salary    INT          NOT NULL
+) engine = InnoDB
+  DEFAULT CHARSET = utf8;
 
